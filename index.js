2d61bf37176276573a8b24afc386907ecd5c061a
Nestor <ncervan87@gmail.com>
2020-06-17 14:57:33 -0700

    minor change to answer on task 4

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ console.log('%c answers to questions can be found on index.js', style)
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. Window binding: This is the default.  When the 'this' keyboard is not being used in the context of an object, it will bind to the window context.
+  1. Window binding: This is the default.  When the 'this' keyboard is not being used in the context of an object, it will bind to the window context.  The window context is the global context where the entire javascript engine exists.
   2. New binding:  When a constructor function is created, the 'this' keyword will be assigned to a new context each time the 'new' keyword is used and will bind itself to the new object that was created from that constructor.
   3. Implicit binding:  The 'this' keyword will automatically refer to the direct object in which it was called. This is going to be the object to the left of the dot in which it was invoked.
      Example: Obj1.funct(), assuming funct() makes use of the 'this' keyword, it will refer to Obj1.  ObjParent.objChild.func(), this will refer to objChild because objChild is the object in which the function was invoked from.  It is implied that 'this' means the immediate context.
